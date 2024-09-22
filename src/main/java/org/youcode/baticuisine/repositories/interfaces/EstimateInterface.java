8e63164b8485f204fb0cf112d89f331acf6e8c40
Aya <ayaelrhayour123@gmail.com>
2024-09-22 19:32:49 +0100

    [BC-48] & [BC-49] created estimte interface

---
+++ src/main/java/org/youcode/baticuisine/repositories/interfaces/EstimateInterface.java
@@ -0,0 +1,14 @@
+package org.youcode.baticuisine.repositories.interfaces;
+
+import org.youcode.baticuisine.entities.Estimate;
+import org.youcode.baticuisine.entities.Project;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface EstimateInterface {
+
+    Optional<Estimate> createEstimate (Estimate estimate);
+    Optional<Estimate> getEstimateByProject (UUID projectId);
+    Optional<Estimate> validateEstimate(UUID id, Estimate estimate);
+}
